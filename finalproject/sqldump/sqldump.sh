086ca1aae174add08eea5bcfcf4d56f2a1668afb
Samir Patel <sp694@njit.edu>
2018-05-09 23:00:03 +0000

    Added Final Script.

--- finalproject/sqldump/sqldump.sh
+++ finalproject/sqldump/sqldump.sh
@@ -8,6 +8,12 @@ backup_path="/home/ubuntu/sqldump"
 # Set default file permissions
 umask 177
 
-# Dump database into SQL file
-mysqldump --user=$user --password=$password client --single-transaction --flush-logs --master-data=2 > $backup_path/full_backup.sql
+date=$(date +"%d-%b-%Y")
+time=$(date +"%T")
 
+
+# Dump database into SQL file
+mysqldump --user=$user --password=$password client --single-transaction --flush-logs --master-data=2 > $backup_path/SQLBackup-$date$time.sql
+
+# Delete files older than 7 days
+find $backup_path/* -mtime +7 -exec rm {} \;
